be4a0082d71c9a6ce84d4de059ea9c62bdbd27b7
v203 <vuyisamr203@gmail.com>
2021-11-25 16:35:35 +0200

    rev-I

---
+++ javascript.js
@@ -0,0 +1,16 @@
+var output = document.querySelector('.outPut');
+
+let Serve = ()=>{
+  let   ourString = (paramName)=>{
+      if(param){
+        return `What we see now is ${paramName}`
+
+    }
+}
+    return{
+        ourString
+
+    }
+}
+
+output.innerHTML
